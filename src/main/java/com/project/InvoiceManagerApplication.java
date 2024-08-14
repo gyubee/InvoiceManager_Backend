05768a9cddd922c49968d529273b5853bf7a3770
임규빈 <gyublayla@gmail.com>
2024-08-15 00:15:01 +1000

    Initial commit

---
+++ src/main/java/com/project/InvoiceManagerApplication.java
@@ -0,0 +1,13 @@
+package com.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InvoiceManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InvoiceManagerApplication.class, args);
+	}
+
+}
